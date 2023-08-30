3bbd32a6f8bcdde01508f0c8efecda4f743f8fdd
Teo <32597645+Sir-Teo@users.noreply.github.com>
2023-08-30 06:00:02 -0400

    updated svg files

--- app/api/mockinfo/works.ts
+++ app/api/mockinfo/works.ts
@@ -2,14 +2,14 @@ export default {
     title: 'Class Projects',
     data: [
         {
-            banner: 'https://images.unsplash.com/photo-1689850543263-01a52ccc6943',
+            banner: '../commonicons/westbrook.jpeg',
             title: ' Is Westbrook a good player in the 2021-22 season?',
             descriptions:
                 'This prjoect use data from NBA.com and 2k.com from 2015 to 2021 to analyze the performance of Russel Westbrook. I used 5 different machine leanring methods to predict the performance of Westbrook in the 2021-22 season. We also used the data to analyze the performance of Westbrook in the 2021-22 season.',
             link: 'https://github.com/Sir-Teo/pstat131-project',
         },
         {
-            banner: 'https://images.unsplash.com/photo-1689850543263-01a52ccc6943',
+            banner: '../commonicons/sarima.jpg',
             title: 'Forcasting the Hospitality Industry',
             descriptions:
                 'In this forecasting assignment, we selected a SARIMA (5,1,2) x (0,1,1)12 model to forecast the number of employees in the Hospitality Industry. By comparing the forecasting values with the actual values, we found that the model overestimated the number of employees, with increasing gaps as time progreeses. We arrived at a conclusion that this model may be improved by using only data after 2010.',
