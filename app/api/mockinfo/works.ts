e2bb90279b3ae75a0c227e9c65fd147a7f098e0b
Teo <32597645+Sir-Teo@users.noreply.github.com>
2023-08-30 06:23:12 -0400

    added poster

--- app/api/mockinfo/works.ts
+++ app/api/mockinfo/works.ts
@@ -12,8 +12,21 @@ export default {
             banner: '../commonicons/sarima.jpg',
             title: 'Forcasting the Hospitality Industry',
             descriptions:
-                'In this forecasting assignment, we selected a SARIMA (5,1,2) x (0,1,1)12 model to forecast the number of employees in the Hospitality Industry. By comparing the forecasting values with the actual values, we found that the model overestimated the number of employees, with increasing gaps as time progreeses. We arrived at a conclusion that this model may be improved by using only data after 2010.',
+                'We selected a SARIMA (5,1,2) x (0,1,1)12 model to forecast the number of employees in the Hospitality Industry. By comparing the forecasting values with the actual values, we found that the model overestimated the number of employees, with increasing gaps as time progreeses. We arrived at a conclusion that this model may be improved by using only data after 2010.',
             link: 'https://github.com/Sir-Teo/pstat-174-final-project',
+        },
+        {
+            banner: '../commonicons/mindfulness.jpeg',
+            title: 'Investigating the Effect of Mindfulness',
+            descriptions: 'Our research question aims to investigate the impact of a single 3-minute guided mindfulness meditation session on reducing state anxiety and test anxiety, as well as enhancing positive affect among college students. Additionally, we are interested in exploring whether self-esteem serves as a moderating factor in this relationship.',
+            poster: '../pdf/Mindfulness Poster.pdf',
+        },
+        {
+            banner: '../commonicons/clifford.png',
+            title: 'Clifford Algebra',
+            descriptions:
+                'In UCSB\'s Directed Reading Program, we examined the basic properties of Clifford Algebra',
+            poster:'../pdf/An_Introduction_to_Clifford_Algebra.pdf',
         },
     ],
     status: 0,
