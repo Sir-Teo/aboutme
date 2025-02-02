232166d86ae5f04fbbd5566b53dd521682d142a7
Teo <32597645+Sir-Teo@users.noreply.github.com>
2025-02-02 00:14:06 -0500

    update project

--- app/api/mockinfo/experience.ts
+++ app/api/mockinfo/experience.ts
@@ -3,7 +3,7 @@ export default {
     data: [
         // Apple
         {
-            logo: './commonicons/apple.svg', // company logo
+            logo: './commonicons/.svg', // company logo
             hireDate: '2021-07', // Date of hire
             lastDay: '2021-9', // Last day of work
             companyFullname: 'Cheese Fund', // Full name of the company
--- app/api/mockinfo/projects.ts
+++ app/api/mockinfo/projects.ts
@@ -2,6 +2,47 @@ export default {
     title: 'Projects',
     data: [
         {
+            url: 'https://github.com/Sir-Teo/teominal',
+            name: 'Teonominal: A Fake Terminal for Viewing Resumes',
+            tags: ['Front-end','xterm.js','Linux','2025'],
+            descriptions: [
+                'Teonominal is a playful, single-page web application that simulates a file-system and terminal environment using xterm.js. Users can run common shell commands like ls, cat, cd, and more—all inside their browser.',
+            ],
+            demo: 'https://sir-teo.github.io/teominal/',
+        },
+        {
+            url: 'https://sir-teo.github.io/teominal/',
+            name: 'My RAG Project',
+            tags: ['Language Models','RAG','vLLM','Gradio','LangChain','2025'],
+            descriptions: [
+                'An custom advanced Retrieval Augmented Generation system built with FAISS, Langchain, vllm, and Gradio)',
+            ],
+        },
+        {
+            url: 'https://sir-teo.github.io/teominal/',
+            name: 'HandTrack-SAM2',
+            tags: ['Segmentation','2025'],
+            descriptions: [
+                'HandTrack-SAM2 is a Python-based project that implements an automatic pipeline for tracking hand movements in videos.',
+            ],
+        },
+        {
+            url: 'https://github.com/Sir-Teo/AI-Therapist',
+            name: 'AI-Therapist',
+            tags: ['Llama 3.3','LangChain','2024'],
+            descriptions: [
+                'Empathy-driven therapist chatbot based on Llama 3.3',
+            ],
+        },
+        {
+            url: 'https://github.com/Sir-Teo/llama-ao3',
+            name: 'Llama3.3-AO3',
+            tags: ['Llama 3.3','LoRA','2024'],
+            descriptions: [
+                'Finetuning Llama 3.2 on AO3 dataset',
+            ],
+        },
+        {
             url: 'https://github.com/Sir-Teo/MusRec-Web',
             name: 'Music Recommendation Application',
             tags: ['Front-end','Machine Learning','Spotify API','2023'],
